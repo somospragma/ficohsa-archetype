19d9e71a5bd9595d28b64a3b17a3e0a9597a4e9d
david.yepes <david.yepesb@pragma.com.co>
2025-12-17 01:36:51 -0500

    Add logging support to cache and crypto price use cases

--- applications/usecase/src/main/java/co/com/ficohsa/usecase/GetCryptoPriceUseCase.java
+++ applications/usecase/src/main/java/co/com/ficohsa/usecase/GetCryptoPriceUseCase.java
@@ -3,17 +3,20 @@ package co.com.ficohsa.usecase;
 import co.com.ficohsa.domain.ports.out.CryptoPriceGateway;
 import co.com.ficohsa.domain.ports.out.CryptoPriceGateway.CryptoPriceResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 /**
  * Caso de uso: Obtener precio de criptomoneda
  * Usa CryptoPriceGateway (output port del dominio)
  */
+@Slf4j
 @RequiredArgsConstructor
 public class GetCryptoPriceUseCase {
     private final CryptoPriceGateway cryptoPriceGateway;
 
     public Mono<CryptoPriceResponse> execute(String symbol) {
+        log.info("Ejecutando caso de uso: Obtener precio de criptomoneda para el símbolo {}", symbol);
         return cryptoPriceGateway.getCryptoPrice(symbol);
     }
 }
--- applications/usecase/src/main/java/co/com/ficohsa/usecase/cache/GetCacheValueUseCase.java
+++ applications/usecase/src/main/java/co/com/ficohsa/usecase/cache/GetCacheValueUseCase.java
@@ -2,17 +2,20 @@ package co.com.ficohsa.usecase.cache;
 
 import co.com.ficohsa.domain.ports.out.CacheGateway;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 /**
  * Caso de uso: Obtener un valor de caché
  * Usa CacheGateway (output port del dominio)
  */
+@Slf4j
 @RequiredArgsConstructor
 public class GetCacheValueUseCase {
     private final CacheGateway cacheGateway;
 
     public Mono<String> execute(String key) {
+        log.info("Ejecutando caso de uso: Obtener valor de caché para la clave {}", key);
         return cacheGateway.get(key);
     }
 }
